08c59d145666e2f0f521c11642fb912c81cfc50b
Maasuuum <fardousalmasum09@gmail.com>
2024-02-17 22:46:12 +0600

    miles to km by function

---
+++ milesToKm.js
@@ -0,0 +1,11 @@
+function milesToKm(miles){
+    const km = miles * 1.609;
+    return km;
+}
+const miles1 = 100;
+const km1 = milesToKm(miles1);
+console.log(km1);
+
+const miles2 = 150;
+const km2 = milesToKm(miles2);
+console.log(km2);
